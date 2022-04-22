a9f078f140e31e50a870de63ccb21a20e5af302f
IAvecilla <iavecilla@fi.uba.ar>
2022-04-22 16:05:45 -0300

    implement public/private rsa api

---
+++ src/api.rs
@@ -0,0 +1,86 @@
+use rsa::{RsaPrivateKey, RsaPublicKey, PaddingScheme, PublicKey, errors::Error};
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct RSAKeyPair {
+    pub public_key: RsaPublicKey,
+    pub private_key: RsaPrivateKey,
+}
+
+pub fn _encrypt(public_key: &RsaPublicKey, data: &[u8]) -> Result<Vec<u8>, Error> {
+    let mut rng = rand::thread_rng();
+    public_key.encrypt(&mut rng, PaddingScheme::new_pkcs1v15_encrypt(), &data)
+}
+
+pub fn _decrypt(private_key: &RsaPrivateKey, data: &[u8]) -> Result<Vec<u8>, Error> {
+    private_key.decrypt(PaddingScheme::new_pkcs1v15_encrypt(), data)
+}
+
+pub fn _sign(private_key: &RsaPrivateKey, data: &[u8]) -> Result<Vec<u8>, Error> {
+    private_key.sign(PaddingScheme::new_pkcs1v15_sign(None), &data)
+} 
+
+pub fn _verify_sign(public_key: &RsaPublicKey, data: &[u8], signature: &[u8]) -> Result<(), Error> {
+    public_key.verify(PaddingScheme::new_pkcs1v15_sign(None), data, &signature)
+}
+
+pub fn generate_key_pair(bits: usize) -> Result<RSAKeyPair, Error> {
+    let mut rng = rand::thread_rng();
+    let private_key = RsaPrivateKey::new(&mut rng, bits)?;
+    let public_key = RsaPublicKey::from(&private_key);
+    Ok(RSAKeyPair {private_key, public_key})
+}
+
+#[cfg(test)]
+mod api_tests {
+    use crate::{generate_key_pair, _encrypt, _decrypt, _sign, _verify_sign, RSAKeyPair};
+
+    fn setup_test() -> (RSAKeyPair, &'static [u8]) {
+        let keys: RSAKeyPair = generate_key_pair(2048).unwrap();
+        let message = "secret";
+
+        (keys, message.as_bytes())
+    }
+
+    #[test]
+    fn test01_a_message_can_be_decrypted_with_a_the_private_key_associated_with_the_public_key_that_encrypted_it() {
+        let (keys, message): (RSAKeyPair, &'static [u8]) = setup_test();
+        let encrypted_message = _encrypt(&keys.public_key, message).unwrap();
+        
+        let decrypted = _decrypt(&keys.private_key, &encrypted_message).unwrap();
+
+        assert_eq!(message, decrypted.as_slice());
+    }
+    
+    #[test]
+    fn test02_a_message_cannot_be_decrypted_with_a_private_key_that_is_not_associated_with_the_public_key_that_encrypted_it() {
+        let (keys, message): (RSAKeyPair, &'static [u8]) = setup_test();
+        let (keys_2, _): (RSAKeyPair, &'static [u8]) = setup_test();
+        let encrypted_message = _encrypt(&keys.public_key, message).unwrap();
+
+        let decrypted = _decrypt(&keys_2.private_key, &encrypted_message);        
+
+        assert!(decrypted.is_err());
+    }
+
+    #[test]
+    fn test03_messages_signed_with_a_private_key_can_be_verified_with_the_corresponding_public_key() {
+        let (keys, message): (RSAKeyPair, &'static [u8]) = setup_test();
+        let signed_message = _sign(&keys.private_key, message).unwrap();
+        
+        let verified = _verify_sign(&keys.public_key, message, &signed_message);
+
+        assert!(verified.is_ok());
+    }
+
+    #[test]
+    fn test04_messages_signed_with_a_private_key_cannot_be_verified_with_any_public_keys() {
+        let (keys, message): (RSAKeyPair, &'static [u8]) = setup_test();
+        let (keys_2, _): (RSAKeyPair, &'static [u8]) = setup_test();
+        let signed_message = _sign(&keys.private_key, message).unwrap();
+
+        let verified = _verify_sign(&keys_2.public_key, message, &signed_message);
+
+        assert!(verified.is_err());
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,58 @@
-use rsa::{RsaPrivateKey, RsaPublicKey, PaddingScheme, PublicKey};
-use serde::{Serialize, Deserialize};
-use wasm_bindgen::prelude::*;
+use rsa::{RsaPublicKey, RsaPrivateKey};
+use wasm_bindgen::{prelude::*, JsCast};
 
-#[derive(Serialize, Deserialize)]
-struct RSAKeyPair {
-    public_key: RsaPublicKey,
-    private_key: RsaPrivateKey,
+mod api;
+use api::*;
+
+#[wasm_bindgen]
+pub fn generate_key_pair_str(bits: usize) -> Result<String, JsError> {
+    if let Ok(key_pair) = generate_key_pair(bits) {
+        match serde_json::to_string(&key_pair) {
+            Ok(key_pair_str) => Ok(key_pair_str),
+            Err(err) => Err(JsError::from(err)),
+        }
+    } else {
+        Err(JsError::new("Failed to generate key pair"))
+    }   
 }
 
 #[wasm_bindgen]
-pub fn generate_key_pair(bits: usize) -> String {
-    let mut rng = rand::thread_rng();
-    let private_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
-    let public_key = RsaPublicKey::from(&private_key);
-    let key_pair = RSAKeyPair {private_key, public_key};
-    serde_json::to_string(&key_pair).unwrap()
+pub fn encrypt(public_key: &str, data: &[u8]) -> Result<Vec<u8>, JsError> {
+    let public_key: RsaPublicKey = serde_json::from_str(&public_key).unwrap();
+    let encrypted = _encrypt(&public_key, data)?;
+    Ok(encrypted)
 }
 
 #[wasm_bindgen]
-pub fn encrypt(public_key: &str, data: &[u8]) -> Vec<u8> {
-    let mut rng = rand::thread_rng();
-    let public_key: RsaPublicKey = serde_json::from_str(public_key).unwrap();
-    public_key.encrypt(&mut rng, PaddingScheme::PKCS1v15Encrypt, &data).unwrap()
+pub fn decrypt(private_key: &str, data: &[u8]) -> Result<Vec<u8>, JsError> {
+    let private_key : RsaPrivateKey = serde_json::from_str(&private_key).unwrap();
+    let decripted = _decrypt(&private_key, data)?;
+    Ok(decripted)
 }
 
 #[wasm_bindgen]
-pub fn decrypt(private_key: &str, data: &[u8]) -> Vec<u8> {
-    let private_key: RsaPrivateKey = serde_json::from_str(private_key).unwrap();
-    private_key.decrypt(PaddingScheme::PKCS1v15Encrypt, data).expect("Failed to decrypt")
-}
-
-
-#[wasm_bindgen]
-pub fn sign(private_key: &str, data: &[u8]) -> Vec<u8> {
-    let private_key: RsaPrivateKey = serde_json::from_str(private_key).unwrap();
-    private_key.sign(PaddingScheme::PKCS1v15Encrypt, &data).unwrap()
+pub fn sign(private_key: &str, data: &[u8]) -> Result<Vec<u8>, JsError> {
+    let private_key : RsaPrivateKey = serde_json::from_str(&private_key).unwrap();
+    let signature = _sign(&private_key, data)?;
+    Ok(signature)
 } 
 
-// TODO: Verify signature
 #[wasm_bindgen]
 pub fn verify_sign(public_key: &str, data: &[u8], signature: &[u8]) -> bool {
-    let public_key: RsaPublicKey = serde_json::from_str(public_key).unwrap();
-    public_key.verify(PaddingScheme::PKCS1v15Encrypt, data, &signature).is_ok()
+    let public_key : RsaPublicKey = serde_json::from_str(&public_key).unwrap();
+    _verify_sign(&public_key, data, signature).is_ok()
 }
+
+
 
 // Called when the wasm module is instantiated
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
-    // Use `web_sys`'s global `window` function to get a handle on the global
-    // window object.
-    let window = web_sys::window().expect("no global `window` exists");
-    let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
-
-    // Manufacture the element we're gonna append
-    let val = document.create_element("p")?;
-    val.set_inner_html("Hello from Rust!");
-
-    body.append_child(&val)?;
-
     Ok(())
 }
 
 #[cfg(test)]
-mod tests {
-    // TODO: Make some tests
+mod wasm_tests {
+    // TODO: make tests
+    // #[wasm_bindgen_test]
 }
